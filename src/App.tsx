4942f6dbd0ddebd84f802c41c5e64aad0d534ead
guan <guan@guandeMacBook-Pro.local>
2020-08-01 23:40:27 +0800

    自定义hook1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import Hello from './components/Hello'
-import LinkButton from './components/LinkButton'
+import useMousePosition from './hooks/useMousePosition'
 import './App.scss'
 
 function App() {
+  const [showMouseTracker, setShowMouseTracker] = useState(true)
+  const positions = useMousePosition()
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <button onClick={() => setShowMouseTracker(!showMouseTracker)}>
+            toggle showMouseTracker
+          </button>
+        </p>
         <Hello />
-        <LinkButton />
+        <p>
+          x:{positions.x}，y:{positions.y}
+        </p>
       </header>
     </div>
   )
--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const LinkButton: React.FC = () => {
   const [like, setLike] = useState(0)
   const [on, setOn] = useState(true)
 
+  useEffect(() => {
+    console.log('触发linkbutton的useEffect')
+    document.title = `点击了${like}次`
+  }, [like])
+
   return (
     <React.Fragment>
-      <button onClick={() => setLike(like + 1)}>
-        {like}
-      </button>
+      <button onClick={() => setLike(like + 1)}>{like}</button>
       <button onClick={() => setOn(!on)}>{on ? '开' : '关'}</button>
+      <div className="gsp"></div>
     </React.Fragment>
   )
 }
