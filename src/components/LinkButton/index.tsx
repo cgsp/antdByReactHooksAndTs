5b0416eb0da06dac94f40a0891ba7366a0ea78a4
guan <guan@guandeMacBook-Pro.local>
2020-08-10 20:39:06 +0800

    提价

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -1,8 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const LinkButton: React.FC = () => {
   const [like, setLike] = useState(0)
   const [on, setOn] = useState(true)
+  const likeRef = useRef(0)
+  const didMountRef = useRef(false)
+
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.log('现在是更新阶段，执行吧')
+    } else {
+      console.log('初次渲染')
+      didMountRef.current = true
+    }
+  })
 
   useEffect(() => {
     console.log('触发linkbutton的useEffect')
@@ -12,12 +23,20 @@ const LinkButton: React.FC = () => {
   function handleClick() {
     setTimeout(() => {
       console.log(like)
+      console.log(likeRef.current)
     }, 3000)
   }
 
   return (
     <React.Fragment>
-      <button onClick={() => setLike(like + 1)}>{like}</button>
+      <button
+        onClick={() => {
+          setLike(like + 1)
+          likeRef.current++
+        }}
+      >
+        {like}
+      </button>
       <button onClick={() => setOn(!on)}>{on ? '开' : '关'}</button>
       <button onClick={handleClick}>点我试试呀</button>
     </React.Fragment>
