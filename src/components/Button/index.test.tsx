8df7b427dee67aca8d0f901553dee9f8cf0eff01
guan <guan@guandeMacBook-Pro.local>
2020-08-24 19:16:45 +0800

    测试用例结束

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -1,15 +1,63 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import Button from './index'
+import { render, fireEvent } from '@testing-library/react'
+import Button, { ButtonProps, ButtonType, ButtonSize } from './index'
+const defaultProps = {
+  onClick: jest.fn(),
+}
 
-test('button组件测试1', () => {
-  const wrapper = render(<Button>Nice</Button>)
-  const element = wrapper.queryByText('Nice')
-  expect(element).toBeTruthy()
-})
+const testProps: ButtonProps = {
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  className: 'kclass',
+}
 
-test('button组件测试2', () => {
-  const wrapper = render(<Button>Nice1</Button>)
-  const element = wrapper.queryByText('Nice1')
-  expect(element).toBeTruthy()
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn(),
+}
+
+// describe 和 it 解释
+// https://www.cnblogs.com/sampapa/p/6963936.html
+describe('测试 Button 组件', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    // expect(element).toBeTruthy()
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element.disabled).toBeFalsy()
+    expect(element).toHaveClass('btn btn-default')
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toHaveBeenCalled()
+  })
+
+  it('should render the correct component based on dif props', () => {
+    const wrapper = render(<Button {...testProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    // expect(element).toBeTruthy()
+    expect(element).toBeInTheDocument()
+    expect(element).toHaveClass('btn-primary btn-lg kclass')
+  })
+
+  it('should render a link when btnType equals link and href is provided', () => {
+    const wrapper = render(
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+        Link
+      </Button>
+    )
+    const element = wrapper.getByText('Link')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('A')
+    expect(element).toHaveClass('btn btn-link')
+  })
+
+  it('should render disabled button when disabled set to true', () => {
+    const wrapper = render(<Button {...disabledProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    // expect(element).toBeTruthy()
+    expect(element).toBeInTheDocument()
+    expect(element.disabled).toBeTruthy()
+    fireEvent.click(element)
+    expect(disabledProps.onClick).not.toHaveBeenCalled()
+  })
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,8 @@
+/**
+ * 这个文件是 jest-dom 专门配合做单元测试的
+ */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect'
