d97abab0fa29e6b413c5f1c7c99d4907160a31fa
guan <guan@guandeMacBook-Pro.local>
2020-09-30 17:53:18 +0800

    提交book

--- src/stories/OfficialExample/Button/Button.stories.tsx
+++ src/stories/OfficialExample/Button/Button.stories.tsx
@@ -10,11 +10,37 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  // 增加参数，来控制顶部的工具栏，选择颜色
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: 'red', value: '#f00' },
+        { name: 'green', value: '#0f0' },
+        { name: 'blue', value: '#00f' },
+        { name: '#f40', value: '#f40' },
+      ],
+    },
+  },
+  // 装饰器
+  decorators: [
+    (Story) => (
+      <div className="gsp" style={{ padding: '30px', background: 'red' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
+// 也可以这么写=======================不用Template=====================================
+// export const PrimaryTest: React.FC<{}> = () => (
+//   <Button primary={true} label="PrimaryTest" />
+// )
+
 export const Primary = Template.bind({})
+
+Primary.storyName = '默认组件Primary📚📕📈🤓'
 Primary.args = {
   primary: true,
   label: 'Button',
